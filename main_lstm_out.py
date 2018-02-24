2c059c99b977c304d420b82fcdb935d0d3456593
bmaneesh <mbilalpur@gmail.com>
2018-02-24 16:51:07 +0800

    cuda OOM error

--- main_lstm_out.py
+++ main_lstm_out.py
@@ -26,10 +26,12 @@ all_data = np.load('iemocap_sixclass.npz')
 data = all_data['data']
 data = np.reshape(data,(data.shape[0], data.shape[1], 1))
 targets = all_data['label']
-class_num = targets.max()
-targets_onehot = np.zeros((targets.shape[0], class_num+1))
-for sample in enumerate(targets):
-    targets_onehot[sample[0], sample[1]] = 1
+class_num = targets.max() + 1
+# targets_onehot = np.zeros((targets.shape[0], class_num))
+targets_onehot = np.reshape(targets, (targets.shape[0], 1))
+# class_num = 1
+# for sample in enumerate(targets):
+#     targets_onehot[sample[0], sample[1]] = 1
 data_shape = data.shape
 # Normalize
 # data = (data - data.mean())/data.std()
@@ -59,15 +61,18 @@ class lstm_att(nn.Module):
         # drop_out = self.dropout(lstm_out)
         alpha = self.atten(lstm_out)
         # print alpha.size(), hidden[0].size()
-        contxt = torch.matmul(alpha.transpose_(1,2), lstm_out)
+        # contxt = torch.matmul(alpha.transpose_(1,2), lstm_out)
+        contxt = torch.matmul(alpha.view(alpha.size()[0], 1, alpha.size()[1]), lstm_out)
         # print contxt.size()
-        out = self.sigmoid(self.dense_out(contxt))
+        out = torch.squeeze(self.sigmoid(self.dense_out(contxt)))
         # print out
         return out
 
     def atten(self, hid):
+        # print 'hidden shape:{0}'.format(hid.size())
         U = self.dense(hid)
-        alpha = self.soft(self.tanh(U))
+        # print 'U-shape:{0}'.format(U.size())
+        alpha = self.soft(self.tanh(U).view(U.size()[0], U.size()[1]))
         return alpha
 
 LEARNING_RATE = 0.01
@@ -77,22 +82,22 @@ BATCH_SIZE = 64
 # input = Variable(torch.randn(1000,70000,1))#1000 batch size, 100 seq len
 inputs = torch.from_numpy(data).type(FloatTensor)
 targets = Variable(torch.from_numpy(targets_onehot).type(LongTensor))
-print inputs.type, targets.type
+# print inputs.type, targets.type
 model = lstm_att(input_size = 1,num_layers = 1, hidden_size = 20, drop_rate = 0.3, dense_out=10, align_hidd=10,class_num=class_num)
 print model.parameters()#, inputs[0*BATCH_SIZE:(0+1)*BATCH_SIZE,].shape
 
 loss_function = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr = LEARNING_RATE)
 mean = [-0.5329]
-std = [1302.4]#torch.std(input)
+std = [1302.4]# torch.std(input)
 norm = Normalize(mean, std)
 inputs = Variable(norm(inputs.permute(2,0,1)).permute(1,2,0))
 # print mean, std
 
 if cuda:
     model = model.cuda()
-    # inputs = inputs.cuda()
-    # targets = targets.cuda()
+    inputs = inputs.cuda()
+    targets = targets.cuda()
     print 'GPU enabled'
 
 for epoch in range(NEPOCHS):
@@ -100,12 +105,13 @@ for epoch in range(NEPOCHS):
     print 'epoch-{0}/{1}'.format(epoch, NEPOCHS)
     for bid, bind in enumerate(batch(data_shape[0], BATCH_SIZE)):
         binputs, btargets = inputs[bind,:,:], targets[bind,:]
-        if cuda:
-            binputs.cuda()
-            btargets.cuda()
+        # binputs, btargets = inputs[bind,:,:], targets[bind]
+        # if cuda:
+        #     binputs.cuda()
+        #     btargets.cuda()
         pred = model(binputs, istraining = True)
-        model.zero_grads()
-        loss = loss_function(pred,btargets)
+        model.zero_grad()
+        loss = loss_function(pred, torch.squeeze(btargets))
         losses.append(loss.data.tolist()[0])
         loss.backward()
         optimizer.step()
